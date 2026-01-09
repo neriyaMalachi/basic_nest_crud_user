676b095e1e4b0f93d8b8d90748dadc4e79b8cce0
Neriya Malachi <neriamalachi@gmail.com>
2026-01-09 14:49:23 +0200

    add prisma ORM for DB with SQLite

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,18 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
-  getAllData() {
+
+  findAll() {
     return this.prisma.product.findMany();
   }
-  async GetById(id: number) {
+
+  async findOne(id: number) {
     const product = await this.prisma.product.findUnique({ where: { id } });
-    if (!product) {
-      throw new NotFoundException('products not found');
-    }
+    if (!product) throw new NotFoundException('Product not found');
+    return product;
   }
-  createProduct(data: { name: string; price: number }) {
+
+  create(data: { name: string; price: number; stock?: number }) {
     return this.prisma.product.create({ data });
+  }
+
+  update(id: number, data: { name?: string; price?: number; stock?: number }) {
+    return this.prisma.product.update({ where: { id }, data });
+  }
+
+  remove(id: number) {
+    return this.prisma.product.delete({ where: { id } });
   }
 }
